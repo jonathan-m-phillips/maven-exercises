1ede27f07059ea585c33bd09e8d9f72ed2dbeb91
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-08-31 15:57:37 -0500

    Maven Exercise project in process, 8/31

--- src/main/java/MavenExercises.java
+++ src/main/java/MavenExercises.java
@@ -1,5 +1,20 @@
+import com.google.common.base.Splitter;
+import com.google.common.math.DoubleMath;
+import org.apache.commons.codec.binary.StringUtils;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class MavenExercises {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        String userInput = scan.nextLine();
+        try {
+            int userNum = Integer.parseInt(userInput);
+        } catch (Exception e) {
+            System.err.println("This is not a number.");
+        }
 
     }
 }
